eab8b21434e55c9b23730958d1af0039ee9007a4
sebashmakov <sebashmakov@ozon.ru>
2022-02-13 16:46:44 +0300

    database description

--- migrations/1_initial_data.sql
+++ migrations/1_initial_data.sql
@@ -1,6 +1,41 @@
+-- +goose Up
+-- +goose StatementBegin
 create table users
 (
-    id integer primary key,
+    id SERIAL primary key,
     name varchar not null,
     telegram_id integer not null
 );
+
+create table rool
+(
+    id  SERIAL primary key,
+    name varchar not null
+);
+
+create table rool_users
+(
+    id  SERIAL primary key,
+    user_id integer not null,
+    rool_id integer not null
+);
+
+INSERT INTO rool (name)
+VALUES ('admin');
+
+INSERT INTO rool (name)
+VALUES ('user');
+
+INSERT INTO users (name, telegram_id)
+VALUES ( 'MrS','519588080');
+
+INSERT INTO rool_users (user_id, rool_id)
+VALUES ('1', '1');
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table if exists users;
+drop table if exists rool;
+drop table if exists rool_users;
+-- +goose StatementEnd
